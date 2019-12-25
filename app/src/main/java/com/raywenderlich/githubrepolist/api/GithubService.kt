452a914f345e6212d7cac8dceddd503c2af3f8ae
Fuad Kamal <fuad@anaara.com>
2019-12-25 00:25:54 -0500

    simplified service
    don’t need `Call` object wrapping anymore

--- app/src/main/java/com/raywenderlich/githubrepolist/api/GithubService.kt
+++ app/src/main/java/com/raywenderlich/githubrepolist/api/GithubService.kt
@@ -1,13 +1,12 @@
 package com.raywenderlich.githubrepolist.api
 
 import com.raywenderlich.githubrepolist.data.RepoResult
-import retrofit2.Call
 import retrofit2.http.GET
 
 interface GithubService {
     @GET("/repositories")
-    suspend fun retrieveRepositories(): Call<RepoResult>
+    suspend fun retrieveRepositories(): RepoResult
 
     @GET("/search/repositories?q=language:kotlin&sort=stars&order=desc") //sample search
-    suspend fun searchRepositories(): Call<RepoResult>
+    suspend fun searchRepositories(): RepoResult
 }
